7aec4e2d7a4a4307e9fb584aaa21959680088e33
Runpeng Hu <72607370+runpenghu@users.noreply.github.com>
2025-09-15 10:34:33 -0400

    update ahi harmonized in script

--- scripts/prepare-pvdomics-for-nsrr.R
+++ scripts/prepare-pvdomics-for-nsrr.R
@@ -37,11 +37,11 @@ df <- df |>
 # setdiff(colnames(df), unlist(selectedvars))
 
 
-###NSRR Harmonized:
+###NSRR Harmonized:c
 df_h <- df|>
   select(alt_pid, age, female, race, hispanic, bmi, smoke, ahi_c, oahi_c, cai_c, odi3p, odi4p, tot_sleep_tm)|> #they also have age_sleep: age during sleep study 
   rename(nsrrid = alt_pid,
-         nsrr_ahi_hp3u = ahi_c,
+         nsrr_ahi_hp3n = ahi_c,
          nsrr_oahi_hp3u = oahi_c,
          nsrr_cai = cai_c,
          nsrr_odi_dsge3 = odi3p,
@@ -68,6 +68,8 @@ df_h <- df|>
                                           0 ~ "no",
                                           1 ~ "yes"))|>
   select(-c(age, bmi, female, race, hispanic, smoke))|>
-  relocate(nsrr_ahi_hp3u, nsrr_oahi_hp3u, nsrr_cai,
+  relocate(nsrr_ahi_hp3n, nsrr_oahi_hp3u, nsrr_cai,
            nsrr_odi_dsge3, nsrr_odi_dsge4, nsrr_tst_f1,
            .after = last_col())
+
+write.csv(df_h, "/Volumes/bwh-sleepepi-nsrr-staging/20241025-pvdomics/nsrr-prep/_releases/0.1.0.pre/pvdomics-harmonized-dataset-0.1.0.csv")
