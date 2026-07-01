1e7f6993fc287dad9907736bb96861878e0ee2b0
Runpeng Hu <72607370+runpenghu@users.noreply.github.com>
2026-07-01 13:15:43 -0400

    updated dd with additional clinical vars

--- scripts/prepare-pvdomics-for-nsrr.R
+++ scripts/prepare-pvdomics-for-nsrr.R
@@ -1,44 +1,49 @@
-version <- "0.1.0.pre3"
+version <- "0.1.0.pre5"
 releasepath <- "/Volumes/bwh-sleepepi-nsrr-staging/20241025-pvdomics/nsrr-prep/_releases"
 
-library(tidyverse)
+library(tidyver)
 
-# selectedvars <- c(
-#   "GENDER","FEMALE","HISPANIC","RACE","BMI","FC","PH_PREV","F_STATUS",
-#   "F_WNOWT","F_WNOWT_C","F_GROUP","F_COMPGRP","PH_AGE_DIAG","PH_YRS","MI",
-#   "CAB","ATRIAL_ARRHY","CARDIAC_SHUNT","SHUNT_PFO","SHUNT_ASD","SHUNT_VSD",
-#   "SHUNT_SEPTOSTOMY","SHUNT_STATUS","TIA","CVA","HYPERTENSION","DIABETES",
-#   "HYPOTHYROIDISM","AIRWAY_DIS","DEPRESSION_YN","SMOKE","CIG_PACK_YRS",
-#   "TIM_ENR_QUITCIG","PH_PDE5I","PH_ERA","PH_PROST","PH_SGC","PH_CCB","PH_MED",
-#   "CARDIAC_MED","ANTIARRHYTHMICS","ANTICOAGULANTS_DOACS","ANTICOAGULANTS_OTHER",
-#   "ANTIHYPERTENSIVES_OTHER","REST_O2","REST_LPM","NIGHT_O2","NIGHT_LPM",
-#   "EXERTION_O2","EXERTION_LPM","CPAP_USED","PVDSLEEP","O2_NIGHT","AHI_C",
-#   "OAHI_C","CAI_C","PCTLT90_C","ODI3P","ODI4P","TOT_SLEEP_TM","HI_C","C_OSA","C_OHS") |> 
-#   tolower()
-# 
-# view(selectedvars)
 
 datapath <- "/Volumes/bwh-sleepepi-nsrr-staging/20241025-pvdomics/original-data/nsrr_rel1noft_d04302024_v04152025.csv"
 
 df <- read.csv(datapath) |>
-  select(-PID)   #deidentify, use alt_pid instead
-
-df <- df |>
+  select(-PID) |>  #deidentify, use alt_pid instead
   rename_with(tolower)|> 
   mutate(visit = 1,
          across(where(is.character), ~na_if(., "")))|>
   relocate(alt_pid, .before = 1)|>
   relocate(visit, .before = 2)|>
   arrange(alt_pid) |>
-  filter(alt_pid != "GD5322") #delete one ID with outlier tst
+  mutate( # remove undefined values
+    tot_sleep_tm = case_when( # remove total sleep time for PID 410204 (outlier total sleep time, no sleep report available)
+      tot_sleep_tm == 4748 ~ NA,
+      TRUE ~ tot_sleep_tm))
 
 
-# unique(df$f_group)
-# sum(is.na(df$f_group))
-# #?? for 'f_group' there is a '.' category, should i change this to "0" or something else?
-# 
-# setdiff(selectedvars, colnames(df))
-# setdiff(colnames(df), unlist(selectedvars))
+###------- Adding release 2 variables to the main df: --------##
+
+datapath2 <- "/Volumes/bwh-sleepepi-nsrr-staging/20241025-pvdomics/original-data/rel2-20260609/PVD_NSRR_rel2_noft_d02052026_v05282026.csv"
+
+df2 <- read.csv(datapath2) |>
+  select(-PID) |>  #deidentify, use alt_pid instead
+  rename("alt_pid" = "pidd") |>
+  rename_with(tolower)
+
+df_joined <- full_join(df, df2, by = "alt_pid", suffix = c("", ".df2"))
+
+overlap_cols <- intersect(names(df), names(df2))
+overlap_cols <- setdiff(overlap_cols, "alt_pid")
+
+for (col in overlap_cols) {
+  df_joined[[col]] <- coalesce(df_joined[[col]], df_joined[[paste0(col, ".df2")]])
+}
+
+df_joined <- df_joined |>
+  select(-all_of(paste0(overlap_cols, ".df2")))
+
+
+write.csv(df_joined, file.path(releasepath, paste0(version, "/pvdomics-dataset-", version, ".csv")), na = "", row.names = F)
+
 
 
 ###NSRR Harmonized:
@@ -76,33 +81,8 @@ df_h <- df|>
   relocate(nsrr_rei_hp3n,nsrr_odi_dsge3, nsrr_odi_dsge4, 
            .after = last_col())
 
-write.csv(df, file.path(releasepath, paste0(version, "/pvdomics-dataset-", version, ".csv")), na = "", row.names = F)
 write.csv(df_h, file.path(releasepath, paste0(version, "/pvdomics-harmonized-dataset-", version, ".csv")), na = "", row.names = F)
 
 
 
-
-###########
-###creating a data dictionary for mapping integer vars
-df_lab <- read.csv("/Volumes/bwh-sleepepi-nsrr-staging/20241025-pvdomics/original-data/nsrr_rel1_d04302024_v04152025.csv", header = T, skip = 1)
-names(df_lab) <- tolower(names(df_lab))
-
-df_ch<- df %>% mutate(across(-alt_pid, as.character))
-df_lab_chr <- df_lab %>% mutate(across(-alt_pid, as.character))
-
-dict <- df_lab_chr %>%
-  pivot_longer(-c(alt_pid,age, age_sleep, ahi_c, bmi, cai_c, cig_pack_yrs, hi_c, oahi_c, odi3p, odi4p, pctlt90_c, ph_age_diag, ph_yrs, tim_enr_quitcig, tot_sleep_tm),
-               names_to = "variable", values_to = "code_chr") %>%
-  full_join(
-    df_lab_chr %>%
-      pivot_longer(-c(alt_pid, pid, age, age_sleep, ahi_c, bmi, cai_c, cig_pack_yrs, hi_c, oahi_c, odi3p, odi4p, pctlt90_c, ph_age_diag, ph_yrs, tim_enr_quitcig, tot_sleep_tm),
-                   names_to = "variable", values_to = "label"),
-    by = c("alt_pid", "variable")
-  ) %>%
-  distinct(variable, code_chr, label) %>%
-  arrange(variable, suppressWarnings(as.numeric(code_chr)), code_chr)
-
-dict <- dict[1:224,]
-
-#write.csv(dict, "/Volumes/bwh-sleepepi-nsrr-staging/20241025-pvdomics/original-data/cat_vars_dict.csv", row.names = F)
 
